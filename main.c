d5a40c840a016105925a3e850ebb1ba9ebf6b531
fulcus <francescofgonzales@gmail.com>
2020-07-13 16:47:26 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main() {
+    char input[10];
+    char *addrString1, *addrString2;
+    char command;
+    int addr1, addr2;
+
+    while (1) {
+        scanf("%s", input);
+
+        command = input[strlen(input) - 1]; //last char of input
+        input[strlen(input) - 1] = '\0'; //deleted command char from input
+
+        //printf("%c %s", command, input);
+
+        //every command:
+        //(address1,address2)c
+        //(address1,address2)d
+        //(address1,address2)p
+        //(number)u
+        //(number)r
+        //q
+
+
+        if (command == 'c') { //change
+            addrString1 = strtok(input, ",");
+            addrString2 = strtok(NULL, "");
+
+            addr1 = atoi(addrString1);
+            addr2 = atoi(addrString2);
+            printf("%d %d %c\n", addr1, addr2, command);
+        } else if (command == 'd') { //delete
+            addrString1 = strtok(input, ",");
+            addrString2 = strtok(NULL, "");
+
+            addr1 = atoi(addrString1);
+            addr2 = atoi(addrString2);
+            printf("%d %d %c\n", addr1, addr2, command);
+        } else if (command == 'p') { //print
+            addrString1 = strtok(input, ",");
+            addrString2 = strtok(NULL, "");
+
+            addr1 = atoi(addrString1);
+            addr2 = atoi(addrString2);
+            printf("%d %d %c\n", addr1, addr2, command);
+        } else if (command == 'u') { //undo
+            addr1 = atoi(input);
+            printf("%d %c\n", addr1, command);
+        } else if (command == 'r') { //redo
+            //numbers is already a single number
+            addr1 = atoi(input);
+            printf("%d %c\n", addr1, command);
+        } else if (command == 'q') { //quit
+            return 0;
+        }
+
+    }
+
+}
